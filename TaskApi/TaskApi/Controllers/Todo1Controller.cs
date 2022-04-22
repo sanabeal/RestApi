12e355a42e5ab3e2b18fc2a74dcf87844a84e2b4
Sanabeal Kabir <sanabeal89@gmail.com>
2022-04-22 23:31:44 +0600

    upload

---
+++ TaskApi/TaskApi/Controllers/Todo1Controller.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TaskApi.Models;
+
+namespace TaskApi.Controllers
+{
+    [RoutePrefix("api/todo1")]
+    public class Todo1Controller : ApiController
+    {
+       // PPLEntities db = new PPLEntities();
+     
+        string constr = ConfigurationManager.ConnectionStrings["PPLEntities"].ConnectionString;
+        [HttpGet]
+        public HttpResponseMessage findData()
+        {
+            SqlDataAdapter adapt;
+            DataTable dt;
+            adapt = new SqlDataAdapter("SELECT OrderId, OrderNo, CustomerName, CustomerMobile, ShippingAddress, CONVERT(CHAR(10), AddDate, 103) AS AddDate FROM eco.[Order]", constr);
+            dt = new DataTable();
+            adapt.Fill(dt);
+            var serializeData = JsonConvert.SerializeObject(dt);
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StringContent(serializeData);
+            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+            return response;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public HttpResponseMessage findData(int Id)
+        {
+            SqlDataAdapter adapt;
+            DataTable dt;
+            adapt = new SqlDataAdapter("SELECT OrderId, OrderNo, CustomerName, CustomerMobile, ShippingAddress, CONVERT(CHAR(10), AddDate, 103) AS AddDate FROM eco.[Order] WHERE (OrderId = '" + Id + "')", constr);
+            dt = new DataTable();
+            adapt.Fill(dt);
+            var serializeData = JsonConvert.SerializeObject(dt);
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StringContent(serializeData);
+            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+            return response;
+        }
+    }
+}
---
+++ TaskApi/TaskApi/Controllers/Todo2Controller.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TaskApi.Models;
+
+namespace TaskApi.Controllers
+{
+    [RoutePrefix("api/todo2")]
+    public class Todo2Controller : ApiController
+    {
+       // PPLEntities db = new PPLEntities();
+        string constr = ConfigurationManager.ConnectionStrings["PPLEntities"].ConnectionString;
+        [HttpGet]
+        public IHttpActionResult findAll()
+        {
+            SqlDataAdapter adapt;
+            DataTable dt;
+            adapt = new SqlDataAdapter("SELECT OrderId, OrderNo, CustomerName, CustomerMobile, ShippingAddress, CONVERT(CHAR(10), AddDate, 103) AS AddDate FROM eco.[Order]", constr);
+            dt = new DataTable();
+            adapt.Fill(dt);
+            var tasks = dt;
+            return Ok(new { Results = tasks });
+        }
+
+        [HttpGet]
+        public IHttpActionResult findData(int Id)
+        {
+            SqlDataAdapter adapt;
+            DataTable dt;
+            adapt = new SqlDataAdapter("SELECT OrderId, OrderNo, CustomerName, CustomerMobile, ShippingAddress, CONVERT(CHAR(10), AddDate, 103) AS AddDate FROM eco.[Order] WHERE (OrderId = '" + Id + "')", constr);
+            dt = new DataTable();
+            adapt.Fill(dt);
+            var tasks = dt;
+            return Ok(new { Results = tasks });
+        }
+    }
+}
